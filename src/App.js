c240c651af4c55282a08671a67e344bfc59b07f6
Pablo <pablito@me.com>
2023-05-09 20:17:32 +0100

    50mins in some errors but code now working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // import logo from './logo.svg';
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([{
+    "id": "1",
+    "text": "Hello"
+  }])
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+const Task = ( {task} ) => {
+  return (
+    <div className='task'>
+      <h3>{task}</h3>
+      {/* <p>{task.day}</p> */}
+    </div>
+  )
+}
+
+export default Task
