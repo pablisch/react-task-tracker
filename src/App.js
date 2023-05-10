166a56efb8f0aef8d1ef08433e977146d1ef62ca
Pablo <pablito@me.com>
2023-05-10 01:04:21 +0100

    1.21 button not changing colour. needs debug

--- src/App.js
+++ src/App.js
@@ -2,18 +2,60 @@
 import { useState } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    "id": "1",
-    "text": "Hello",
-    "day": "now"
-  }])
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+    "id": 1,
+    "text": "One",
+    "day": "before",
+    "reminder": true
+    },
+    {
+    "id": 2,
+    "text": "Two",
+    "day": "now",
+    "reminder": true
+    },
+    {
+    "id": 3,
+    "text": "Three",
+    "day": "after",
+    "reminder": false
+    }
+  ])
+
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    console.log(task)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id)) // sets tasks to all tasks except the one that matches the id of the task being deleted
+    console.log('delete', id)
+  }
+  
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task
+    ))
+    console.log(id)
+  }
+
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header onAdd={() => setShowAddTask(!showAddTask) } />
+      {showAddTask && <AddTask onAdd={addTask} showAdd={showAddTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks}
+        onDelete={deleteTask}
+        onToggle={toggleReminder} /> : <h3>No tasks to show</h3>}
     </div>
   );
 }
